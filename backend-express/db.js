f344079c0070c87ca345c4466baf27c6c2bbd167
Sohan Das <dassohan550@gmail.com>
2026-09-05 02:26:50 +0530

    Debug Render database configuration

--- backend-express/db.js
+++ backend-express/db.js
@@ -1,3 +1,19 @@
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DB_URI });
+const { Pool } = require("pg");
+
+console.log("DB CONFIG:", {
+  user: process.env.DB_U,
+  host: process.env.DB_H,
+  database: process.env.DB_N,
+  port: process.env.DB_PT,
+  passwordSet: !!process.env.DB_P,
+});
+
+const pool = new Pool({
+  user: process.env.DB_U,
+  host: process.env.DB_H,
+  database: process.env.DB_N,
+  password: process.env.DB_P,
+  port: Number(process.env.DB_PT),
+});
+
 module.exports = pool;
